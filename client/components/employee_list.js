d48302da16e298427fd7c9b6ddd9c34951fce363
lau-willis <lauwillis1991@gmail.com>
2017-12-04 11:59:09 -0500

    employee directory now is able to paginate and load 20 if clicked

--- client/components/employee_list.js
+++ client/components/employee_list.js
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {withTracker} from 'meteor/react-meteor-data';
 import {Employees} from '../../imports/collections/employees';
 import EmployeeDetail from './employee_detail';
 
-const EmployeeList = (props) => {
-	return (
-		<div>
-			<div className="employee-list">
-			{props.employees.map((employee) => <EmployeeDetail key={employee._id} employee={employee}/>)}
+const PER_PAGE = 20;
+class EmployeeList extends Component{
+	componentWillMount(){
+		this.page = 1;
+	}
+
+	handleButtonClick(){
+		Meteor.subscribe('employees', PER_PAGE * (this.page + 1));
+		this.page += 1;
+	}
+	render(){
+		return (
+			<div>
+				<div className="employee-list">
+				{this.props.employees.map((employee) => <EmployeeDetail key={employee._id} employee={employee}/>)}
+				</div>
+				<button onClick={this.handleButtonClick.bind(this)} className="btn btn-primary">Load More...</button>
 			</div>
-		</div>
-		);
+			);
+	}
 };
 
 export default withTracker(() => {
 	//set up subscription
-	Meteor.subscribe('employees');
+	Meteor.subscribe('employees', PER_PAGE);
 
 	//return an object. whatever we return will be sent to EmployeeList
 	//as props
